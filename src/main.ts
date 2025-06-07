f74f1468c480a7479fe08dbebff4e89967b4dd48
nhuhieu193 <mama82bells@gmail.com>
2025-06-08 01:19:30 +0700

    bug

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 // src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations'; // 🆗 BẮT BUỘC
 
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideAnimations(),
     provideHttpClient(),
-  ]
+    provideRouter(routes),
+    provideAnimations(), // ✅ Fix lỗi HighContrastModeDetector
+  ],
 }).catch(err => console.error(err));
