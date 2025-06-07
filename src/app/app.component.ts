7a1bc9b09aa14e95ced3b82c9249733f5e8b2cc8
nhuhieu193 <mama82bells@gmail.com>
2025-06-08 01:26:58 +0700

    bug2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { MetadataListComponent } from './metadata/metadata-list.component';
 
 @Component({
-  standalone: true,
   selector: 'app-root',
+  standalone: true,
   imports: [MetadataListComponent],
   template: `<app-metadata-list></app-metadata-list>`
 })
